c927e231852ebc8b91e30f6071b50ad5bb77f68d
Tim Barclay <timrbarclay@gmail.com>
2015-09-12 20:10:51 +0100

    Added bg images and overlay

--- client/template_logic/section.js
+++ client/template_logic/section.js
@@ -1,5 +1,15 @@
+Template.section.onRendered({
+
+});
+
 Template.section.helpers({
   background: function(){
+    if(this.backgroundImage){
+      return "background-image: url(" + this.backgroundImage.url + "";
+    }
     return "background-color: " + this.backgroundColor || "#000000";
+  },
+  hasBackgroundImage: function(){
+    return !!this.backgroundImage;
   }
 });
