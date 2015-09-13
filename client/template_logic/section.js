bc35c29dd4df792c627c749506e5a7a1406320a6
Tim Barclay <timrbarclay@gmail.com>
2015-09-13 15:10:24 +0100

    More styling and tidying

--- client/template_logic/section.js
+++ client/template_logic/section.js
@@ -1,13 +1,10 @@
-Template.section.onRendered({
-
-});
-
 Template.section.helpers({
   background: function(){
     if(this.backgroundImage){
       return "background-image: url(" + this.backgroundImage.url + "";
     }
-    return "background-color: " + this.backgroundColor || "#000000";
+    return "background-color: " + this.backgroundColour ||
+      orion.dictionary.get('site.defaultColour', '#ffffff');;
   },
   hasBackgroundImage: function(){
     return !!this.backgroundImage;
